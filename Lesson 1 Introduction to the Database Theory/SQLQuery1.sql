01f63efc25acb39ad077b03815c1eadfebfcb0dc
Tural Novruzov <novruzov_t@itstep.edu.az>
2022-12-02 11:45:15 +0400

    finish

---
+++ Lesson 1 Introduction to the Database Theory/SQLQuery1.sql
@@ -0,0 +1,17 @@
+SELECT * FROM Students 
+
+
+SELECT Id, [Name], Surname FROM Students 
+
+
+SELECT Id, [Name] 
+FROM Students 
+WHERE [Name] = 'Resul' OR Id = 5
+
+
+SELECT Id, [Name] AS FirstName
+FROM Students
+WHERE [Name] LIKE '%e%'
+
+
+
